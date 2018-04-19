8d85cc122a881cbe7e0e99e8630b333948a692f7
Capt-Obviouse <sixstringjesse@gmail.com>
2018-04-18 21:22:17 -0400

    added link to commerical facilities

--- src/components/pages/commercialFacilities.js
+++ src/components/pages/commercialFacilities.js
@@ -11,6 +11,16 @@ class Commercialfacilities extends Component {
             the Cannery Building. Ocean view space is available upstairs. Build-out can be done for qualified tenants.
             Contact the Port of Gold Beach office to discuss details.
           </article>
+          <article>
+            <h2>Current Businesses</h2>
+            <ul>
+              <li>
+                <a href="http://portholecafe.com/" target="_new">
+                  Port Hole Cafe
+                </a>
+              </li>
+            </ul>
+          </article>
         </section>
       </div>
     );
