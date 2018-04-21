addcb7cb1bf3f6f3a1f4441523c4e5f86d7e9c03
Capt-Obviouse <sixstringjesse@gmail.com>
2018-04-21 18:54:11 -0400

    page content updates

--- src/components/pages/portBusiness.js
+++ src/components/pages/portBusiness.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 class Portbusiness extends Component {
   render() {
     return (
@@ -7,8 +7,33 @@ class Portbusiness extends Component {
         <section id="contentstart">
           <article>
             <h2>Port Business</h2>
-            The Port of Gold Beach is entrusted with certain public assets, taxes and authorities to be used for the
-            good of the community.
+            <p>
+              The Port of Gold Beach is entrusted with certain public assets, taxes and authorities to be used for the
+              good of the community.
+            </p>
+          </article>
+          <article>
+            <h2>Information</h2>
+            <ul>
+              <li>
+                <Link to="/Port_Business/Ordinance">Ordinance</Link>
+              </li>
+              <li>
+                <Link to="/Port_Business/Budget">Budget</Link>
+              </li>
+              <li>
+                <Link to="/Port_Business/Meetings">Meetings</Link>
+              </li>
+              <li>
+                <Link to="/Port_Business/Resolutions">Resolutions</Link>
+              </li>
+              <li>
+                <Link to="/Port_Business/Strategic_Plan">Strategic Plan</Link>
+              </li>
+              <li>
+                <Link to="/Port_Business/Airport_Plan">Airport Plan</Link>
+              </li>
+            </ul>
           </article>
         </section>
       </div>
