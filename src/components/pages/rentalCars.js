a891664faf9b8d143f8cddd2019208e00332b8b9
Capt-Obviouse <sixstringjesse@gmail.com>
2018-04-26 08:59:19 -0400

    updated services as per Jacob B. Request. updated rental car page to have appropriate text.

--- src/components/pages/rentalCars.js
+++ src/components/pages/rentalCars.js
@@ -7,9 +7,8 @@ class rentalCars extends Component {
         <section id="contentstart">
           <article>
             <h2>Rental Cars</h2>
-            The Port of Gold Beach has storage available in the Port compound. The Port Compound is a gated area
-            adjacent to the Port Offices. Open, covered, or closed storage are available. RV&#39;s, boats, and trailers
-            may all be stored in the Port Compound Contact the Port Office for prices and availability.
+            The Port of Gold Beach offers several rental cars. For availiablity, pricing and scheduling contact the Port
+            Office. Operators must present a valid drivers license, proof of insurance, and be over the age of 25.
           </article>
         </section>
       </div>
